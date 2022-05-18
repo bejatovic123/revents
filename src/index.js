3f8d548ba96397dc59450c041e208d70d6fdf341
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-05-19 01:21:54 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+// const rootEl = document.getElementById('root');
+
+// function render() {
+//   ReactDOM.render(<App />, rootEl);
+// }
+
+// if (module.hot) {
+//   module.hot.accept('./App', function () {
+//     setTimeout(render);
+//   });
+// }
+
+// render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
